cae3cf759bf61c31503651228d19fa8487532242
Adelana107 <adelana787898@gmail.com>
2025-11-02 19:14:58 +0100

    working with color

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,5 @@
-// prettier.config.js
-
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   plugins: ["prettier-plugin-tailwindcss"],
   singleQuote: true,
-} 
+};
