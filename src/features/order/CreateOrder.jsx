d95c7bb3c8e6781a6081e32df67260cf8e2c0afc
Adelana107 <adelana787898@gmail.com>
2025-11-03 17:18:54 +0100

    reusing styles with @apply

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -62,6 +62,7 @@ function CreateOrder() {
         <div>
           <label>First Name</label>
           <input
+            className="input"
             type="text"
             name="customer"
             required
@@ -73,6 +74,7 @@ function CreateOrder() {
         <div>
           <label>Phone number</label>
           <input
+            className="input"
             type="tel"
             name="phone"
             required
@@ -86,7 +88,7 @@ function CreateOrder() {
           <label>Address</label>
           <div>
             <input
-              className="transition:text-stone-400 focus w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              className="input"
               type="text"
               name="address"
               required
