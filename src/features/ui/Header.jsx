968ed88921b5b0e88633e6751ae2ddbecf0ca8e0
Adelana107 <adelana787898@gmail.com>
2025-11-03 11:17:33 +0100

    styling text

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../order/SearchOrder';
+import Username from '../user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
-
-      <p>Jonas</p>
+      <Username />
     </header>
   );
 }
