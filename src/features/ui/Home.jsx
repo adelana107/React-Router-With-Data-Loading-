a23a8e1edcfd3297bdec97fad060916c7fafa7b5
Adelana107 <adelana787898@gmail.com>
2025-11-03 11:45:40 +0100

    The Box Model Spacing, Borders, and Display

--- src/features/ui/Home.jsx
+++ src/features/ui/Home.jsx
@@ -1,13 +1,16 @@
+import CreatUser from '../user/CreateUser';
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-center text-[300px] font-semibold text-stone-700">
+    <div className="my-10 text-center">
+      <h1 className="mb-8 text-center font-semibold">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
+      <CreatUser />
     </div>
   );
 }
--- src/features/user/username.jsx
+++ src/features/user/username.jsx
@@ -1,5 +1,5 @@
 function username() {
-  return <div className="text-sm font-semibold">Adelana</div>;
+  return <div className="hidden text-sm font-semibold">Adelana</div>;
 }
 
 export default username;
